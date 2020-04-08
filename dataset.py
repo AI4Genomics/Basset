0e5d37c571841b75b7fbcdc784ea05ce1390c274
Ahmad Pesaranghader <ahmadpgh@gmail.com>
2020-04-08 04:13:55 -0400

    Improve dataset.py by adding __main__ if check

--- dataset.py
+++ dataset.py
@@ -14,11 +14,6 @@ device = torch.device("cuda:0" if use_gpu else "cpu")
 print("Torch version: ", torch.__version__)
 print("GPU Available: {}".format(use_gpu))
 
-
-
-train_dataset = BassetDataset(data_path, file_name, 'train_in', 'train_out')
-
-
 class BassetDataset(Dataset):
     # Initializes the BassetDataset
     def __init__(self, path, f5name, sequences, labels, transform=None):
@@ -41,7 +36,7 @@ class BassetDataset(Dataset):
         assert self.samples_len == self.output_len  # testing that samples_len & output_len are same == self.test_shape & self.y.shape are same
         self.train = self.train[:].reshape([self.samples_len, self.n_nucleotides, self.seq_len])
 
-        print(samples"Input shape: {self.train.shape}")
+        print("samples input shape: {}".format(self.train.shape))
         print(self.train[1])
         # samples.close()
 
@@ -59,3 +54,22 @@ class BassetDataset(Dataset):
     def cleanup(self):
         self.samples.close()
 
+
+if __name__ == '__main__': # Notice: this helps to run this script independent from the rest of the project as well
+    import argparse
+    parser = argparse.ArgumentParser() # Notice: list of the arguments for this script, feel free to add more if you think it is needed
+    parser.add_argument('--path', type=str, required=True, help='Path to the dataset directory.')
+    parser.add_argument('--file_name', type=str, required=True, help='Name of the h5 file already preprocessed.')
+    parser.add_argument('--split', type=str, default='train', help='Defines what data split to work with (default=tarin).')
+    args = parser.parse_args()
+    
+    print("Arguments are: ".format(args)) # Notice: this line needs to be removed later; use 'python dataset.py --help' instead (try now!) to get the list of args
+    
+    if args.split=='train': # Notive, you need to complete this based on your preference 
+        print('Preparing Basset Dataset for training phase...')
+    else:
+        pass
+    print('Done')    
+    
+    basset_dataset = BassetDataset(data_path, file_name, 'train_in', 'train_out')
+    # Notice: you must check some tensor shapes and other stuff here (for basset_dataset[0] & basset_dataset[1]) to make sure it's done right above
