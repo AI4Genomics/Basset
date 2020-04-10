bf505b9d8925d9bfb87685970027b38a0ff37fe2
Zoe Schmilovich <zoeschmilovich@Zoes-MacBook-Air.local>
2020-04-10 18:42:30 -0400

    dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -3,22 +3,66 @@
 # It can also apply some sort of transformation to the data if we ask to
 #   (eg, rotating the input images with slight angles in each training iteration so the network would see them as new images and won't overfit)
 
+# Goal: extract 3 separate files -- training, testing, and validation sets
 #train_dataloader = DataLoader(train_dataset, batchsize, shuffle=False, num_workers=data_workers)
 
-import preprocess
-import dataset
-from torch.utils.data import DataLoader
+from torchim.utils.data import DataLoader
 
 #TODO: SIMPLY EXTRACT THE TRAINING, TESTING AND VALIDATION SET ALREADY GENERATED FROM THE PREPROCESSING USING COMMAND LINE
+
+import numpy as np
+import h5py
+
 with h5py.File('er.h5', 'r') as hdf:
     ls = list(hdf.keys())
     print('List of datasets in this file: \n', ls)
-    train_dataset = hdf.get('train_in')
-    test_dataset = hdf.get('train_out')
-    valid_dataset =
-    print("Shape of the input training data is:", train_in.shape)
-    print("Shape of the out training data is:", train_out.shape)
+
+    # getting the shape of the keys in the HDF5 file
+    train_dataset = hdf.get('train_in') # returns the value of train_in key
+    train_dataset_out = hdf.get('train_out')
+    test_dataset = hdf.get('test_in')
+    test_dataset_out = hdf.get('test_out')
+    validation_dataset = hdf.get('valid_in')
+
+    print("Shape of the input training data is:", train_dataset.shape)
+    print("Shape of the out training data is:", train_dataset_out.shape)
+    print("Shape of the input testing data is:", test_dataset.shape)
+    print("Shape of the output testing data is:", test_dataset_out.shape)
+    print("Shape of the validation data is:", validation_dataset.shape)
     print(hdf.get('test_headers')[150])
+    print("\n")
+
+    # listing all of the keys in our file
+    print("They keys in our HDF5 file are: \n")
+    for key in hdf.keys():
+        print(key)
+        # all of the keys in the HDF5 file
+
+    # creating new HDF5 datasets for our train, test, and validation data
+    print("\n")
+    print("Our train/test/validation objects: \n")
+    train_in = hdf["train_in"]
+    print("Dataset train_in: ", train_in)
+    test_in = hdf["test_in"]
+    print("Dataset test_in:", test_in)
+    valid_in = hdf["valid_in"]
+    print("Dataset valid_in:", valid_in)
+
+
+
+# creating Train, Test, Validation objects
+# reading groups in the HDF5 file
+with h5py.File('er.h5', 'r') as hdf:
+    base_items = list(hdf.items())
+    print("Items in the base directory: \n", base_items)
+
+    # # let's see what our target labels are...
+    # target_labels = hdf.get('target_labels')
+    # print("Target_labels:", target_labels)
+    # target_labels_items = list(target_labels.items())
+    # print("Items in Target Labels:", target_labels_items)
+
+
 
 train_dataset_sizes = len(train_dataset)
 num_train_samples = int(0.8 * train_dataset_sizes)
