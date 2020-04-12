f0004d995ee07dbf812c8fd2bd758c47d98f0794
kiristern <kiri.a.stern@gmail.com>
2020-04-12 15:52:40 -0400

    def fnc for one-hot to dna

--- dna.py
+++ dna.py
@@ -52,6 +52,46 @@ def dna_one_hot(seq, seq_len=None, flatten=True):
 
     return seq_vec
 
+
 #test
-seq = "ATAGCT"
-dna_one_hot(seq)
+seq = "ATGCAATTAGCT"
+seq_vec = dna_one_hot(seq)
+print(seq_vec)
+
+
+def vecs2dna(seq_vecs):
+    '''
+
+    Input:
+        seq_vecs:
+    Output:
+        seqs
+    '''
+
+    # possibly reshape
+    if len(seq_vecs.shape) == 2:
+        seq_vecs = np.reshape(seq_vecs, (seq_vecs.shape[0], 4, -1))
+    elif len(seq_vecs.shape) == 4:
+        seq_vecs = np.reshape(seq_vecs, (seq_vecs.shape[0], 4, -1))
+
+    seqs = []
+    for i in range(seq_vecs.shape[0]):
+        seq_list = ['']*seq_vecs.shape[2]
+        for j in range(seq_vecs.shape[2]):
+            if seq_vecs[i,0,j] == 1:
+                seq_list[j] = 'A'
+            elif seq_vecs[i,1,j] == 1:
+                seq_list[j] = 'C'
+            elif seq_vecs[i,2,j] == 1:
+                seq_list[j] = 'G'
+            elif seq_vecs[i,3,j] == 1:
+                seq_list[j] = 'T'
+            elif seq_vecs[i,:,j].sum() == 1:
+                seq_list[j] = 'N'
+            else:
+                print('Malformed position vector: ', seq_vecs[i,:,j], 'for sequence %d position %d' % (i,j), file=sys.stderr)
+        seqs.append(''.join(seq_list))
+    return seqs
+
+#test
+vecs2dna(seq_vec)
