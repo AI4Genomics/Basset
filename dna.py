f8d552fdb23989bf1ac391146ff54761cf7f2361
kiristern <kiri.a.stern@gmail.com>
2020-04-12 16:52:18 -0400

    simpler way to one-hot into array

--- dna.py
+++ dna.py
@@ -1,62 +1,23 @@
 #Methods to load training data
 
-from __future__ import print_function
-import sys
-from collections import OrderedDict
-
 import numpy as np
-import numpy.random as npr
-from sklearn import preprocessing
 
-def dna_one_hot(seq, seq_len=None, flatten=True):
-    """
-    Input:
-      seq
-    Output:
-       seq_vec: Flattened column vector
-    """
-    if seq_len == None:
-        seq_len = len(seq)
-        seq_start = 0
-    else:
-        if seq_len <= len(seq):
-            # trim the sequence
-            seq_trim = (len(seq)-seq_len) // 2
-            seq = seq[seq_trim:seq_trim+seq_len]
-            seq_start = 0
-        else:
-            seq_start = (seq_len-len(seq)) // 2
+#TODO: make sure mapping is the same order used in the preprocessing steps
+def dna_to_onehot(seq_nts):
+    """function that takes a dna sequence of nucleotide and returns onehot-encoded representation of it"""
+    nt_dict = {'A': [1, 0, 0, 0],
+               'C': [0, 1, 0, 0],
+               'G': [0, 0, 1, 0],
+               'T': [0, 0, 0, 1]
+              }
+    return np.asarray([nt_dict[nt.upper()] for nt in seq_nts]).transpose() # transpose to have nb_cols=nb_nts
 
-    seq = seq.upper()
-
-    seq = seq.replace('A','0')
-    seq = seq.replace('C','1')
-    seq = seq.replace('G','2')
-    seq = seq.replace('T','3')
-
-    # map nt's to a matrix 4 x len(seq) of 0's and 1's.
-    #  dtype='int8' fails for N's
-    seq_code = np.zeros((4,seq_len), dtype='float16')
-    for i in range(seq_len):
-        if i < seq_start:
-            seq_code[:,i] = 0.25
-        else:
-            try:
-                seq_code[int(seq[i-seq_start]),i] = 1
-            except:
-                seq_code[:,i] = 0.25
-
-    # flatten and make a column vector 1 x len(seq)
-    if flatten:
-        seq_vec = seq_code.flatten()[None,:]
-
-    return seq_vec
-
-
-#test
+# test
 seq = "ATGCAATTAGCT"
-seq_vec = dna_one_hot(seq)
+seq_vec = dna_to_onehot(seq)
 print(seq_vec)
+
+
 
 
 def vecs2dna(seq_vecs):
